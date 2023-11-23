c0ea65e727c5899835d86a84ee26f4c6cdc16c6c
Bryazgin Sergey <sergeybryazgin@gmail.com>
2023-11-23 23:35:00 +0300

    исправил фикстуры

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,7 +1,7 @@
 from selene import browser, be
 
 
-def test_city_choose_appearance(close_browser):
+def test_city_choose_appearance(close_browser,setup_browser):
     browser.open('')
     browser.element('[class="modalCity_1hLA5 open_23iHa"]').should(be.visible)
 
