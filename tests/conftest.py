7130796176ad0fe605b277fb75ecf96bf0c8f476
Bryazgin Sergey <sergeybryazgin@gmail.com>
2023-12-05 22:08:48 +0300

    добавил изображения для readme

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from utils import attach
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
     options = Options()
     selenoid_capabilities = {
